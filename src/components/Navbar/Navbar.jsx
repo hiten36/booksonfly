4383eb8281bd95a1c482d8f6515db74781999797
hiten <hitenkhatri14@gmail.com>
2023-03-31 20:22:15 +0530

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
                                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                 </div>
-                                <input type="search" id="default-search" className="block w-full p-4 py-3 bg-slate-100 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                                <input type="search" id="default-search" className="block w-full p-4 py-3 bg-slate-100 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Books, Stationary..." required />
                             </div>
                         </form>
 
@@ -83,10 +83,10 @@ const Navbar = () => {
                         </svg>
                         <img onClick={() => {
                             document.getElementById('registerModal').classList.toggle('hidden');
-                        }} src="/images/p7.png" alt="" />
+                        }} src="/images/p5.png" alt="" />
                         <img onClick={() => {
                             document.getElementById('registerModal').classList.toggle('hidden');
-                        }} src="/images/p5.png" alt="" />
+                        }} src="/images/p7.png" alt="" />
                     </div>
                 </div>
             </nav>
